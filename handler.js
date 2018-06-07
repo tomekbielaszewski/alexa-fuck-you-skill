5127b51eed72df9f66a0b232df1da7bcb7e572d8
tbielaszewski <tomekbielaszewski@gmail.com>
2018-06-07 22:38:11 +0100

    alexa swearing skill

---
+++ handler.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+
+module.exports.fuckyou = (event, context, callback) => {
+  const swearWords = [
+    "Fuck you too, you son of a bitch!",
+    "Fuck me? You wish!",
+    "Fuck me if you can, pervert!",
+    "Suck my wires!",
+    "I like it nasty!",
+    "I love you too... HA HA, just kidding you dickhead!"
+  ];
+  const swear = swearWords[Math.floor(Math.random()*swearWords.length)];
+
+  console.log('swearing with: ' + swear);
+  
+  const response = {
+    version: '1.0',
+    response: {
+      outputSpeech: {
+        type: 'PlainText',
+        text: swear,
+      },
+      shouldEndSession: false,
+    },
+  };
+
+  console.log('Alexa response: ' + JSON.stringify(response));
+  callback(null, response);
+};
